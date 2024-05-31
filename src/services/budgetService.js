02e593c233e4995c437561077f88f6cf96d974ea
Alisotoudehpsn4 <alisotoudehpsn4@gmail.com>
2024-05-31 13:10:45 +1000

    test

--- src/services/budgetService.js
+++ src/services/budgetService.js
@@ -1,37 +1,46 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5002/api/budget/';
 
 const getBudgets = () => {
-    return axios.get(`${API_URL}/budgets`, {
-        headers: {
-            'x-auth-token': localStorage.getItem('token'),
-        },
-    });
+    const token = Cookies.get('token');
+    if (token) {
+        return axios.get(`${API_URL}/budgets`, {
+            headers: { 'x-auth-token': token }
+        });
+    }
+    return Promise.reject('No token available');
 };
 
 const addBudget = (budgetData) => {
-    return axios.post(API_URL, budgetData, {
-        headers: {
-            'x-auth-token': localStorage.getItem('token'),
-        },
-    });
+    const token = Cookies.get('token');
+    if (token) {
+        return axios.post(API_URL, budgetData, {
+            headers: { 'x-auth-token': token }
+        });
+    }
+    return Promise.reject('No token available');
 };
 
 const updateBudget = (id, budgetData) => {
-    return axios.put(`${API_URL}/${id}`, budgetData, {
-        headers: {
-            'x-auth-token': localStorage.getItem('token'),
-        },
-    });
+    const token = Cookies.get('token');
+    if (token) {
+        return axios.put(`${API_URL}/${id}`, budgetData, {
+            headers: { 'x-auth-token': token }
+        });
+    }
+    return Promise.reject('No token available');
 };
 
 const deleteBudget = (id) => {
-    return axios.delete(`${API_URL}/${id}`, {
-        headers: {
-            'x-auth-token': localStorage.getItem('token'),
-        },
-    });
+    const token = Cookies.get('token');
+    if (token) {
+        return axios.delete(`${API_URL}/${id}`, {
+            headers: { 'x-auth-token': token }
+        });
+    }
+    return Promise.reject('No token available');
 };
 
 const budgetService = {
